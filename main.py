48d44a9cec319121c0c03e43866afeeb147c19ac
Damon Cai <20635117+Oneiriac@users.noreply.github.com>
2020-09-04 14:45:13 +1000

    Fix timer display getting out of sync; add buzzer; move servo to background

--- main.py
+++ main.py
@@ -1,38 +1,62 @@
-# 30 minute timer
 # Number of minutes remaining scrolls on the screen
-# Servo only has 180 degrees of motion
-# ticks along every 5 mins
-# When timer expires, buzzer sounds
-# press button A for 5 minutes of break and to reset the servo
-# During this time the timer ticks along every 1 minute
-# When break timer expires, another buzzer
-# Press button A to silence the buzzer and to start another 30 minute timer
+# Initially, buzzer is sounding: press A to silence buzzer
+# Press A again to start a work timer
+# When work timer expires, buzzer sounds
+# Press button A to silence the buzzer and reset the servo 
+# Press A again to start a break timer
+# When break timer expires, buzzer sounds
+# Press button A to silence the buzzer 
+# Press A again to start another work timer
 
 # Define constants
-WORK_TIMER_LENGTH = 30
-WORK_TIMER_SERVO_MODULO = 5
-BREAK_TIMER_LENGTH = 10
-BREAK_TIMER_SERVO_MODULO = 2
+WORK_TIMER_LENGTH = 20
+WORK_TIMER_SERVO_MODULO = 2
+BREAK_TIMER_LENGTH = 5
+BREAK_TIMER_SERVO_MODULO = 1
 MAX_SERVO_ANGLE = 180
 TIME_STEP = 1000  # increment that the timer uses
 # Initialise variables
-time_left = WORK_TIMER_LENGTH
-is_work_timer = True
-servo_angle = 0  # max 180
+time_left = 0
+is_work_timer = False  # Will switch to start with work timer after pressing A
+alarm_active = True
+servo_angle = 180  # 0 <= servo_angle <= 180
 
 def display():
+    previous_time_left = -1
     while True:
         if time_left == 0:
-            basic.show_string("!")
+            basic.show_string("!!!", 50) if alarm_active else basic.show_icon(IconNames.ASLEEP)
+            basic.pause(50)
+        else:
+            if time_left != previous_time_left:
+                # Only do basic.show_number once per increment to stop it getting out of sync
+                basic.show_number(time_left, 30)
+                previous_time_left = time_left
+            basic.pause(50)
+
+def buzz():
+    while True:
+        if time_left == 0 and alarm_active:
+            pins.digital_write_pin(DigitalPin.P0, 1)
             basic.pause(300)
-            basic.show_string("")
+            pins.digital_write_pin(DigitalPin.P0, 0)
             basic.pause(300)
         else:
-            basic.show_number(time_left)
             basic.pause(100)
 
-control.in_background(display)
+def set_servo():
+    servo_modulo = 0
+    while True:
+        servo_modulo = WORK_TIMER_SERVO_MODULO if is_work_timer else BREAK_TIMER_SERVO_MODULO
+        if time_left % servo_modulo == 0:
+            pins.servo_write_pin(AnalogPin.P11, servo_angle)
+        basic.pause(100)
 
+
+
+control.in_background(display)
+control.in_background(buzz)
+control.in_background(set_servo)
 
 def main_loop():
     global time_left
@@ -48,28 +72,31 @@ def main_loop():
         # Do a time step
         basic.pause(TIME_STEP)
         time_left -= 1
-        # Servo control
         servo_angle = (current_timer_length-time_left)/current_timer_length*180
-        if time_left % servo_modulo == 0:
-            pins.servo_write_pin(AnalogPin.P11, servo_angle)
 
-    
-    if time_left == 0:
+    if time_left == 0 and not alarm_active:
         servo_angle = 0
-        pins.servo_write_pin(AnalogPin.P11, servo_angle)
 
 basic.forever(main_loop)
 
 def on_button_pressed_a():
     global time_left
     global is_work_timer
+    global alarm_active
+
     if time_left > 0:
         return
+    if alarm_active:
+        alarm_active = False
+        return
+
+    # Triggers when countdown finishes
     if is_work_timer:  # switch to break timer
         is_work_timer = False
         time_left = BREAK_TIMER_LENGTH
-    else:
+    else:  # switch to work timer
         is_work_timer = True
         time_left = WORK_TIMER_LENGTH
+    alarm_active = True
 
 input.on_button_pressed(Button.A, on_button_pressed_a)
--- main.ts
+++ main.ts
@@ -1,37 +1,67 @@
-//  30 minute timer
 //  Number of minutes remaining scrolls on the screen
-//  Servo only has 180 degrees of motion
-//  ticks along every 5 mins
-//  When timer expires, buzzer sounds
-//  press button A for 5 minutes of break and to reset the servo
-//  During this time the timer ticks along every 1 minute
-//  When break timer expires, another buzzer
-//  Press button A to silence the buzzer and to start another 30 minute timer
+//  Initially, buzzer is sounding: press A to silence buzzer
+//  Press A again to start a work timer
+//  When work timer expires, buzzer sounds
+//  Press button A to silence the buzzer and reset the servo 
+//  Press A again to start a break timer
+//  When break timer expires, buzzer sounds
+//  Press button A to silence the buzzer 
+//  Press A again to start another work timer
 //  Define constants
-let WORK_TIMER_LENGTH = 30
-let WORK_TIMER_SERVO_MODULO = 5
-let BREAK_TIMER_LENGTH = 10
-let BREAK_TIMER_SERVO_MODULO = 2
+let WORK_TIMER_LENGTH = 20
+let WORK_TIMER_SERVO_MODULO = 2
+let BREAK_TIMER_LENGTH = 5
+let BREAK_TIMER_SERVO_MODULO = 1
 let MAX_SERVO_ANGLE = 180
 let TIME_STEP = 1000
 //  increment that the timer uses
 //  Initialise variables
-let time_left = WORK_TIMER_LENGTH
-let is_work_timer = true
-let servo_angle = 0
-//  max 180
+let time_left = 0
+let is_work_timer = false
+//  Will switch to start with work timer after pressing A
+let alarm_active = true
+let servo_angle = 180
+//  0 <= servo_angle <= 180
 control.inBackground(function display() {
+    let previous_time_left = -1
     while (true) {
         if (time_left == 0) {
-            basic.showString("!")
+            alarm_active ? basic.showString("!!!", 50) : basic.showIcon(IconNames.Asleep)
+            basic.pause(50)
+        } else {
+            if (time_left != previous_time_left) {
+                //  Only do basic.show_number once per increment to stop it getting out of sync
+                basic.showNumber(time_left, 30)
+                previous_time_left = time_left
+            }
+            
+            basic.pause(50)
+        }
+        
+    }
+})
+control.inBackground(function buzz() {
+    while (true) {
+        if (time_left == 0 && alarm_active) {
+            pins.digitalWritePin(DigitalPin.P0, 1)
             basic.pause(300)
-            basic.showString("")
+            pins.digitalWritePin(DigitalPin.P0, 0)
             basic.pause(300)
         } else {
-            basic.showNumber(time_left)
             basic.pause(100)
         }
         
+    }
+})
+control.inBackground(function set_servo() {
+    let servo_modulo = 0
+    while (true) {
+        servo_modulo = is_work_timer ? WORK_TIMER_SERVO_MODULO : BREAK_TIMER_SERVO_MODULO
+        if (time_left % servo_modulo == 0) {
+            pins.servoWritePin(AnalogPin.P11, servo_angle)
+        }
+        
+        basic.pause(100)
     }
 })
 basic.forever(function main_loop() {
@@ -51,33 +81,36 @@ basic.forever(function main_loop() {
         //  Do a time step
         basic.pause(TIME_STEP)
         time_left -= 1
-        //  Servo control
         servo_angle = (current_timer_length - time_left) / current_timer_length * 180
-        if (time_left % servo_modulo == 0) {
-            pins.servoWritePin(AnalogPin.P11, servo_angle)
-        }
-        
     }
-    if (time_left == 0) {
+    if (time_left == 0 && !alarm_active) {
         servo_angle = 0
-        pins.servoWritePin(AnalogPin.P11, servo_angle)
     }
     
 })
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     
     
+    
     if (time_left > 0) {
         return
     }
     
+    if (alarm_active) {
+        alarm_active = false
+        return
+    }
+    
+    //  Triggers when countdown finishes
     if (is_work_timer) {
         //  switch to break timer
         is_work_timer = false
         time_left = BREAK_TIMER_LENGTH
     } else {
+        //  switch to work timer
         is_work_timer = true
         time_left = WORK_TIMER_LENGTH
     }
     
+    alarm_active = true
 })
